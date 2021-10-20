d966fd011b1b67a0ef7349a1298d35758c3bd3c5
PawelKwidzinski <solvap83@gmail.com>
2021-10-20 17:32:47 +0200

    Adding entity: Currency

---
+++ src/main/java/pl/kwidzinski/curencyexchanger/model/Currency.java
@@ -0,0 +1,42 @@
+package pl.kwidzinski.curencyexchanger.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "currencies")
+@Data
+@NoArgsConstructor
+public class Currency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+    private String symbol;
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @OneToMany(mappedBy = "currency", fetch = FetchType.LAZY)
+    private List<CurrencyRate> currencyRates;
+
+    public Currency(final String symbol, final String name, final LocalDate date) {
+        this.symbol = symbol;
+        this.name = name;
+        this.date = date;
+    }
+}
