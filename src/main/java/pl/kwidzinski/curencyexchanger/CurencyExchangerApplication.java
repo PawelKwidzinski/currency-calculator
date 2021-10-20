8bfb120c737b440217e61b41102c4e09c045d045
PawelKwidzinski <solvap83@gmail.com>
2021-10-20 17:31:15 +0200

    Initial commit

---
+++ src/main/java/pl/kwidzinski/curencyexchanger/CurencyExchangerApplication.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.curencyexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurencyExchangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurencyExchangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kwidzinski/curencyexchanger/CurencyExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.curencyexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyExchangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
