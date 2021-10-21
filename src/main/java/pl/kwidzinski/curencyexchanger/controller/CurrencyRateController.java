ad5fc7514f43d858169843cc9914e3f0c7c775f5
PawelKwidzinski <solvap83@gmail.com>
2021-10-21 11:45:34 +0200

    Adding CurrencyRateController with methods addCurrencyRate()

---
+++ src/main/java/pl/kwidzinski/curencyexchanger/controller/CurrencyRateController.java
@@ -0,0 +1,23 @@
+package pl.kwidzinski.curencyexchanger.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.kwidzinski.curencyexchanger.model.dto.CurrencyRateDTO;
+import pl.kwidzinski.curencyexchanger.service.CurrencyRateService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/currencies/rates")
+@RequiredArgsConstructor
+public class CurrencyRateController {
+    private final CurrencyRateService currencyRateService;
+
+    @PostMapping
+    public void addCurrencyRate(@RequestBody @Valid CurrencyRateDTO dto) {
+        currencyRateService.addRate(dto);
+    }
+}
