4357c6d375050809089031a951ea4fb80fb7251e
PawelKwidzinski <solvap83@gmail.com>
2021-10-22 10:04:56 +0200

    Adding changes in validation messages

--- src/main/java/pl/kwidzinski/curencyexchanger/model/dto/CurrencyDTO.java
+++ src/main/java/pl/kwidzinski/curencyexchanger/model/dto/CurrencyDTO.java
@@ -1,16 +1,23 @@
 package pl.kwidzinski.curencyexchanger.model.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Getter
+@Setter
 public class CurrencyDTO {
 
-    @NotNull(message = "Currency symbol should be no null")
+    @NotNull(message = "Quantity for currency should be not null")
+    @Positive(message = "Quantity parameter should be a positive digit")
+    private Integer quantity;
+
+    @NotNull(message = "Currency symbol should be not null")
     private String symbol;
 
-    @NotNull(message = "Creation date should be no null")
+    @NotNull(message = "Creation date should be not null")
     private LocalDate creationDate;
 }
