141cb519ca6ed21dacf92b0f4151f2af49d936f4
PawelKwidzinski <solvap83@gmail.com>
2021-10-22 15:55:36 +0200

    Rename to ObjectFoundException

--- src/main/java/pl/kwidzinski/curencyexchanger/exceptions/definition/ObjectFoundException.java
+++ src/main/java/pl/kwidzinski/curencyexchanger/exceptions/definition/ObjectFoundException.java
@@ -3,9 +3,9 @@ package pl.kwidzinski.curencyexchanger.exceptions.definition;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class EntityFoundException extends RuntimeException {
+public class ObjectFoundException extends RuntimeException {
 
-    public EntityFoundException(Class<?> clazz, Object... params) {
+    public ObjectFoundException(Class<?> clazz, Object... params) {
         super("Entity: " + clazz.getSimpleName() + " couldn't save because is found using parameters: " + Arrays.stream(params)
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ")));
