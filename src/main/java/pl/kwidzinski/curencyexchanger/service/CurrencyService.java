593726796b4368e835384c80d61b00b6556823cb
PawelKwidzinski <solvap83@gmail.com>
2021-10-22 16:09:08 +0200

    Adding methods: updateCurrency(), deleteCurrency(), getCurrencyRateFromCurrencyRates, rateSymbolExistsInCurrencyRates() and update save() and calculateCurrency()

--- src/main/java/pl/kwidzinski/curencyexchanger/service/CurrencyService.java
+++ src/main/java/pl/kwidzinski/curencyexchanger/service/CurrencyService.java
@@ -2,15 +2,17 @@ package pl.kwidzinski.curencyexchanger.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.kwidzinski.curencyexchanger.exceptions.definition.ObjectFoundException;
+import pl.kwidzinski.curencyexchanger.exceptions.definition.ObjectNotFoundException;
 import pl.kwidzinski.curencyexchanger.model.Currency;
 import pl.kwidzinski.curencyexchanger.model.CurrencyRate;
 import pl.kwidzinski.curencyexchanger.model.CurrencySymbol;
 import pl.kwidzinski.curencyexchanger.model.dto.CurrencyCalculationDTO;
 import pl.kwidzinski.curencyexchanger.model.dto.CurrencyDTO;
+import pl.kwidzinski.curencyexchanger.model.dto.CurrencyUpdateDTO;
+import pl.kwidzinski.curencyexchanger.repository.CurrencyRateRepository;
 import pl.kwidzinski.curencyexchanger.repository.CurrencyRepository;
 import pl.kwidzinski.curencyexchanger.repository.CurrencySymbolRepository;
-
-import javax.persistence.EntityNotFoundException;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,6 +25,7 @@ import static pl.kwidzinski.curencyexchanger.model.dto.CurrencyCalculationDTO.bu
 public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
+    private final CurrencyRateRepository currencyRateRepository;
     private final CurrencySymbolRepository currencySymbolRepository;
 
     public List<Currency> getCurrencies() {
@@ -31,26 +34,32 @@ public class CurrencyService {
 
     public void save(final CurrencyDTO dto) {
         CurrencySymbol currencySymbol = currencySymbolRepository.findBySymbol(dto.getSymbol().toUpperCase())
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Currency symbol %s not found", dto.getSymbol())));
+                .orElseThrow(() -> new ObjectNotFoundException(CurrencySymbol.class, "symbol = " + dto.getSymbol().toUpperCase()));
 
-        Currency currency = new Currency();
+        final boolean existsByDateAndSymbol = currencyRepository.existsByDateAndSymbol(dto.getCreationDate(), currencySymbol.getSymbol());
 
-        currency.setQuantity(dto.getQuantity());
-        currency.setSymbol(currencySymbol.getSymbol());
-        currency.setName(currencySymbol.getName());
-        currency.setDate(dto.getCreationDate());
-        currencyRepository.save(currency);
+        if (!existsByDateAndSymbol) {
+            Currency currency = new Currency();
+
+            currency.setQuantity(dto.getQuantity());
+            currency.setSymbol(currencySymbol.getSymbol());
+            currency.setName(currencySymbol.getName());
+            currency.setDate(dto.getCreationDate());
+            currencyRepository.save(currency);
+        } else {
+            throw new ObjectFoundException(Currency.class, "date = " +dto.getCreationDate(), "symbol = " +currencySymbol.getSymbol());
+        }
     }
 
     public CurrencyCalculationDTO calculateCurrency(final String date, final String baseSymbol, final int quantity, final String calculateSymbol) {
         Currency currency = findByDateAndSymbol(date, baseSymbol)
-                .orElseThrow(() -> new EntityNotFoundException("Currency not found"));
+                .orElseThrow(() -> new ObjectNotFoundException(Currency.class, date, baseSymbol));
 
         CurrencyRate currencyRate = currency.getCurrencyRates()
                 .stream()
                 .filter(rate -> rate.getSymbol().equals(calculateSymbol))
-                .findFirst().orElseThrow(() -> new EntityNotFoundException(
-                        String.format("Currency rate %s for %s not found", calculateSymbol, baseSymbol)));
+                .findFirst()
+                .orElseThrow(() -> new ObjectNotFoundException(CurrencyRate.class, "from " + baseSymbol, "to = " + calculateSymbol));
 
         double result = currency.getQuantity() * quantity * currencyRate.getRate();
 
@@ -59,5 +68,65 @@ public class CurrencyService {
 
     public Optional<Currency> findByDateAndSymbol(final String date, final String symbol) {
         return currencyRepository.findByDateAndSymbol(LocalDate.parse(date), symbol);
+    }
+
+    public void updateCurrency(final Long id, final CurrencyUpdateDTO updateDTO) {
+        Currency currencyFromDB = currencyRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(Currency.class, id));
+
+        CurrencySymbol mainCurrencySymbolFromDB = currencySymbolRepository.findBySymbol(updateDTO.getMainSymbol().toUpperCase())
+                .orElseThrow(() -> new ObjectNotFoundException(CurrencySymbol.class, "main symbol = " + updateDTO.getMainSymbol().toUpperCase()));
+
+        CurrencySymbol currencyRateSymbolFromDB = currencySymbolRepository.findBySymbol(updateDTO.getRateSymbol().toUpperCase())
+                .orElseThrow(() -> new ObjectNotFoundException(CurrencySymbol.class, "rate symbol = " + updateDTO.getRateSymbol().toUpperCase()));
+
+        if (!rateSymbolExistsInCurrencyRates(currencyFromDB, mainCurrencySymbolFromDB.getSymbol())) {
+            currencyFromDB.setDate(updateDTO.getDate());
+            currencyFromDB.setSymbol(mainCurrencySymbolFromDB.getSymbol());
+            currencyFromDB.setName(mainCurrencySymbolFromDB.getName());
+            currencyRepository.save(currencyFromDB);
+        } else {
+            throw new ObjectFoundException(Currency.class, mainCurrencySymbolFromDB.getSymbol());
+        }
+
+        if (rateSymbolExistsInCurrencyRates(currencyFromDB, currencyRateSymbolFromDB.getSymbol())) {
+            CurrencyRate rateToEditFromCurrency = getCurrencyRateFromCurrencyRates(currencyFromDB, currencyRateSymbolFromDB);
+            Long rateIdToUpdate = rateToEditFromCurrency.getId();
+
+            CurrencyRate currencyRateFromDB = currencyRateRepository.findById(rateIdToUpdate)
+                    .orElseThrow(() -> new ObjectNotFoundException(CurrencyRate.class, "id = " + rateIdToUpdate));
+
+            currencyRateFromDB.setSymbol(updateDTO.getRateSymbol());
+            currencyRateFromDB.setRate(updateDTO.getRate());
+
+            currencyRateRepository.save(currencyRateFromDB);
+        } else {
+            throw new ObjectNotFoundException(CurrencyRate.class,
+                    "symbol = " + currencyRateSymbolFromDB.getSymbol(), "symbol = " + currencyFromDB.getSymbol());
+        }
+    }
+
+    public void deleteCurrency(final Long id) {
+        Currency currencyToDelete = currencyRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(Currency.class, id));
+
+        List<CurrencyRate> currencyRates = currencyToDelete.getCurrencyRates();
+
+        currencyRateRepository.deleteAll(currencyRates);
+        currencyRepository.delete(currencyToDelete);
+    }
+
+    private CurrencyRate getCurrencyRateFromCurrencyRates(final Currency currencyFromDB, final CurrencySymbol currencyRateSymbolFromDB) {
+        return currencyFromDB.getCurrencyRates().stream()
+                .filter(rate -> rate.getSymbol().equals(currencyRateSymbolFromDB.getSymbol()))
+                .findFirst()
+                .orElseThrow(() -> new ObjectNotFoundException(
+                        CurrencyRate.class, "symbol = " + currencyFromDB.getSymbol(), "symbol = " + currencyRateSymbolFromDB.getSymbol()));
+    }
+
+    public boolean rateSymbolExistsInCurrencyRates(final Currency currencyFromDB, final String rateSymbolFromDB) {
+        return currencyFromDB.getCurrencyRates().stream()
+                .map(CurrencyRate::getSymbol)
+                .anyMatch(s -> s.equals(rateSymbolFromDB));
     }
 }
