27745a70b19d39f84f6519a558c5b57aea89a299
PawelKwidzinski <solvap83@gmail.com>
2021-10-22 15:56:15 +0200

    Adding method: handleCurrencyNotFound()

--- src/main/java/pl/kwidzinski/curencyexchanger/exceptions/RestExceptionHandler.java
+++ src/main/java/pl/kwidzinski/curencyexchanger/exceptions/RestExceptionHandler.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import pl.kwidzinski.curencyexchanger.exceptions.definition.EntityFoundException;
+import pl.kwidzinski.curencyexchanger.exceptions.definition.ObjectFoundException;
+import pl.kwidzinski.curencyexchanger.exceptions.definition.ObjectNotFoundException;
 import pl.kwidzinski.curencyexchanger.exceptions.response.CurrencyMessageException;
 
 import java.time.LocalDateTime;
@@ -12,9 +13,20 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(value = {EntityFoundException.class})
-    public ResponseEntity<Object> handleCurrencySymbolFound(EntityFoundException ex) {
+    @ExceptionHandler(value = {ObjectFoundException.class})
+    public ResponseEntity<Object> handleCurrencySymbolFound(ObjectFoundException ex) {
         HttpStatus badRequestStatus = HttpStatus.FOUND;
+        CurrencyMessageException currencyMessageException = new CurrencyMessageException(
+                ex.getMessage(),
+                badRequestStatus,
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(currencyMessageException, badRequestStatus);
+    }
+
+    @ExceptionHandler(value = {ObjectNotFoundException.class})
+    public ResponseEntity<Object> handleCurrencyNotFound(ObjectNotFoundException ex) {
+        HttpStatus badRequestStatus = HttpStatus.NOT_FOUND;
         CurrencyMessageException currencyMessageException = new CurrencyMessageException(
                 ex.getMessage(),
                 badRequestStatus,
