0dbae36093b1596fb32948d3d5dc08bd58e7ee7d
PawelKwidzinski <solvap83@gmail.com>
2021-10-22 16:07:25 +0200

    Adding methods for Patch and Delete

--- src/main/java/pl/kwidzinski/curencyexchanger/controller/CurrencySymbolController.java
+++ src/main/java/pl/kwidzinski/curencyexchanger/controller/CurrencySymbolController.java
@@ -2,7 +2,10 @@ package pl.kwidzinski.curencyexchanger.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +33,17 @@ public class CurrencySymbolController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void addCurrencySymbol(@RequestBody @Valid CurrencySymbolDTO dto) {
         currencySymbolService.saveCurrencySymbol(dto);
+    }
+
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void addCurrencySymbol(@PathVariable Long id, @RequestBody @Valid CurrencySymbolDTO dto) {
+        currencySymbolService.updateCurrencySymbol(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void addCurrencySymbol(@PathVariable Long id) {
+        currencySymbolService.deleteCurrencySymbol(id);
     }
 }
